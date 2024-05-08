1ad1498e0494a9bf43438712955a9eb01b127613
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-05-08 22:15:21 +0530

    Update route.ts

--- src/app/api/settings/route.ts
+++ src/app/api/settings/route.ts
@@ -23,7 +23,6 @@ export async function POST(request: Request) {
 
     return NextResponse.json(updatedUser);
   } catch (error: any) {
-    console.log(error, "ERROR_SETTINGS");
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
